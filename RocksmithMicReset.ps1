2f8a539965ef042041cd1a6223abd38979994a89
Joseph Esmaail <josephesmaail93@gmail.com>
2021-12-31 22:07:01 +0000

    Add some output messages to the script

--- RocksmithMicReset.ps1
+++ RocksmithMicReset.ps1
@@ -1,8 +1,12 @@
+Write-Output "Rocksmith Mic Reset running..."
 
 if (!(Get-Module -ListAvailable -Name AudioDeviceCmdlets))
 {   
+    Write-Output "Installing Required Powershell Module."
     Install-Module -Name AudioDeviceCmdlets
 }
 
+Write-Output "Setting Default Recording Device Volume to 100."
 Set-AudioDevice -RecordingVolume 100
 
+Write-Output "Rocksmith Mic Reset Complete!"
